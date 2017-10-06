220bdbc78aa5671ab4653a4ebd6b90c2e29fa758
David Espana <despana.developer@gmail.com>
2017-10-06 01:50:40 -0400

    target scrolling added, in order to give body will add extra images to give page more body

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+const gulp = require('gulp');
+// dependencies should go here
+
+gulp.task('default', function(){
+ //tasks go here
+});
--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 //main.js
 
 let yo = function (){
-  console.log ("we here brahhh");
+  console.log ("fooBar");
   alert ("JS loaded");
 }
 
 yo();
+
+
+$('ul.nav').find('a').click(function(){
+    var $href = $(this).attr('href');
+    var $anchor = $('#'+$href).offset();
+    window.scrollTo($anchor.left,$anchor.top);
+    return false;
+});
