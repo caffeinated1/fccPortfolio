7f253ada51e6e3e52a49e236ba20686d3364fcf6
David Espana <despana.developer@gmail.com>
2017-10-08 20:56:47 -0400

    added some extra zest to the navbar and body

--- main.js
+++ main.js
@@ -9,8 +9,19 @@ yo();
 
 
 $('ul.nav').find('a').click(function(){
-    var $href = $(this).attr('href');
-    var $anchor = $('#'+$href).offset();
+    let $href = $(this).attr('href');
+    let $anchor = $('#'+$href).offset();
     window.scrollTo($anchor.left,$anchor.top);
     return false;
 });
+
+
+// Word Title changer
+const words = ['Grateful', 'Humbled', 'Amazed'];
+// Function that executes every 2000 milliseconds
+let t = setInterval(function() {
+  // Random number generator
+  let randomNumber = Math.round( Math.random() * (words.length-1) );
+  // Change the word in the span for a random one in the array of words
+  $('#changing').html( words[ randomNumber ] );
+}, 1500);
